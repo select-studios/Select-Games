ece66a6606d692b4e3e0742c5af9bbbba00a7236
SimplyUnstoppable <69221317+SimplyUnstoppable@users.noreply.github.com>
2022-07-03 00:58:14 +1000

    CONTACT GO UGHH DADDY CHILL

---
+++ index.js
@@ -0,0 +1,35 @@
+let isModalOpen = false;
+const scaleFactor = 1 / 20;
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_yrto9fq",
+      "template_p0t9eia",
+      event.target,
+      "user_u34ArOptN2qv_M1Ri"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact us directly on help.selectstudios@gmail.com"
+      );
+    });
+}
+
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+}
