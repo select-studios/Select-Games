0540762f8f8e3fa4aedf87e569c9cbc707a910e3
SimplyUnstoppable <69221317+LucasGeorgy@users.noreply.github.com>
2022-08-25 17:41:29 +1000

    react setup

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import RouterPage from './RouterPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
