7e53ec68b24f1ed3bfec1b7fa771afcb715eac48
Myles "Mellurboo" Wilson <mellurblur@gmail.com>
2023-05-29 15:24:51 +0100

    Removed prisma and navbar because for some reason it needed auth to work?

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import cx from "classnames";
 import { sfPro, inter } from "./fonts";
-import Nav from "@/components/layout/nav";
 import Footer from "@/components/layout/footer";
 import { Suspense } from "react";
 
@@ -23,8 +22,6 @@ export default async function RootLayout({
       <body className={cx(sfPro.variable, inter.variable)}>
         <div className="fixed h-screen w-full bg-gradient-to-br from-purple-200 via-white to-blue-200" />
         <Suspense fallback="...">
-          {/* @ts-expect-error Server Component */}
-          <Nav />
         </Suspense>
         <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
           {children}
